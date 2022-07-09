f1a65da2b5d0599a276881dcce91a86ae634ec96
KukinIvan <Provokatorx3@gmail.com>
2022-07-09 13:24:53 +0300

    add file from selenium import webdriver.py

---
+++ from selenium import webdriver.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+from time import sleep
+
+driver = webdriver.Chrome("C:\GIT\Skillfactory\python_selenium_sf\chromedriver")
