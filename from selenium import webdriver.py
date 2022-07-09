9319bce5ffb5f1ddb0230f005f2c8bc19c293024
KukinIvan <Provokatorx3@gmail.com>
2022-07-09 13:44:31 +0300

    modified from selenium import webdriver.py and add chromedriver.exe

--- from selenium import webdriver.py
+++ from selenium import webdriver.py
@@ -4,4 +4,9 @@ from selenium.webdriver.common.keys import Keys
 
 from time import sleep
 
-driver = webdriver.Chrome("C:\GIT\Skillfactory\python_selenium_sf\chromedriver")
+driver = webdriver.Chrome("C:\GIT\Skillfactory\testGITinstruction\chromedriver")
+driver.get("http://google.com")
+driver.find_element(By.XPATH,"/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input").send_keys('facebook' + Keys.RETURN)
+sleep(2)
+driver.save_screenshot('sf.png')
+driver.quit()
